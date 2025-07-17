19d0101ffea579b3e1612f81edd9fd07bea28971
arisrada <arisradad2003@mail.com>
2025-07-17 17:34:15 +0530

    Added env file

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const API_URL = "https://api.spoonacular.com/recipes";
 
-const yourApiKey = "dc6ebc470a0b47d78fd80ed4b7093a81";
+const yourApiKey = process.env.SPOONACULAR_API_KEY;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
